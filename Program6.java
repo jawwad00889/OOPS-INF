1a3354351fc704af00e7614a4085a0c9ee5c0af2
jawwad00889 <keepitweard@gmail.com>
2025-08-09 11:19:00 +0530

    Program 6

---
+++ Program6.java
@@ -0,0 +1,14 @@
+package OOPS;
+
+public class Program6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(5-2); //3
+	      System.out.println(5*2); //10
+	      System.out.println(5/2); //2
+	      System.out.println(5%2); //1
+	}
+
+}
