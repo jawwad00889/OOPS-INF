ed338d8924b68b0183c9b1bca8a596f0fce00584
jawwad00889 <keepitweard@gmail.com>
2025-08-09 11:01:34 +0530

    Program 1

---
+++ demobox.java
@@ -0,0 +1,32 @@
+package OOPS;
+
+import OOPS.demobox.Box;
+
+public class demobox {
+
+    // Class to represent a box
+    static class Box {
+        double width;
+        double height;
+        double depth;
+
+        // Constructor
+        Box(double w, double h, double d) {
+            width = w;
+            height = h;
+            depth = d;
+        }
+
+        // Method to calculate volume
+        double volume() {
+            return width * height * depth;
+        }
+    }
+
+    public static void main(String[] args) {
+        Box mybox = new Box(10, 20, 15);
+        double vol = mybox.volume();
+
+        System.out.println("Volume is " + vol);
+    }
+}
