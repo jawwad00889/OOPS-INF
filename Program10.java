841d0ddc1fa2b0984c177901530cf457fc8f4dc9
jawwad00889 <keepitweard@gmail.com>
2025-08-09 11:20:19 +0530

    Program 10

---
+++ Program10.java
@@ -0,0 +1,16 @@
+package OOPS;
+
+public class Program10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int area;
+	      
+	      int PI=2,radius=2;
+	      
+	         area = radius * radius * PI;
+	        System.out.println("The area for the circle of radius " +radius + " is " + area);
+	}
+
+}
