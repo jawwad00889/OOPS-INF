bd7185da80ce88fb554fbcf99d18e808b3d600c6
jawwad00889 <keepitweard@gmail.com>
2025-08-09 11:19:19 +0530

    Program 7

---
+++ Program7.java
@@ -0,0 +1,15 @@
+package OOPS;
+
+public class Program7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int w,x,y,z;
+	    w = 10;
+	    x = 20;
+	    y = w+x;
+	    z = y;
+	}
+
+}
