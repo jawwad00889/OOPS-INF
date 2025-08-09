c77d7ad50adec8f0aef38620392b167303f6f2d1
jawwad00889 <keepitweard@gmail.com>
2025-08-09 11:19:34 +0530

    Program 8

---
+++ Program8.java
@@ -0,0 +1,16 @@
+package OOPS;
+
+public class Program8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(5<2); 
+	       System.out.println(5<=2); 
+	     System.out.println(5>2); 
+	     System.out.println(5>=2); 
+	     System.out.println(5==2); 
+	     System.out.println(5!=2);
+	}
+
+}
