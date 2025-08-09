753c59d54dc969d38ca1ebb5d702210fd84b9fb7
jawwad00889 <keepitweard@gmail.com>
2025-08-09 11:19:48 +0530

    Program 9

---
+++ Program9.java
@@ -0,0 +1,13 @@
+package OOPS;
+
+public class Program9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=45;
+	    int y=54;
+	    System.out.println((x>y)?x:y);
+	}
+
+}
